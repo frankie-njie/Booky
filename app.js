25c3721b8f4a08e993015dc1e7a996711e3b1d0c
frankie-njie <njie_frankie@yahoo.com>
2021-01-25 12:59:24 +0100

    changes to files

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
+const multer = require('multer');
 
 const app = express();
 
+// Store uploaded files
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -13,7 +25,19 @@ app.get("/" , function(req, res){
     res.render("home");
 });
 
+app.post("/", function(req, res){
+     let upload = multer({ storage}).single('csv-file');
+     console.log(upload);
+     res.render("file")
+     res.send("Your files has been saved");
 
+    //  res.render("file",function(req, res){
+    //     res.send("Your files has been saved");
+    //  })
+ 
+})
+
+//${__dirname}/public/${req.file.filename}
 
 
 app.listen(3000, function(){
