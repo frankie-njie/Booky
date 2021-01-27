8fde49a976f3ee096dff7d14e07e01ac593d2dc2
frankie-njie <njie_frankie@yahoo.com>
2021-01-27 13:13:12 +0100

    some changes

--- app.js
+++ app.js
@@ -3,8 +3,25 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const multer = require('multer');
+const csv = require('csvtojson');
+const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+
+mongoose.connect('mongodb://localhost:27017/BookyDB', {useNewUrlParser: true, useUnifiedTopology: true});
+// TODO: define contact schema
+const bookySchema = {
+    fName : String,
+    lName : String,
+    email : String,
+    phoneNum : Number,
+    Sex : String
+}
+
+const Bookycontact = mongoose.model('Bookycontact', bookySchema);
 
 // Store uploaded files
 const storage = multer.diskStorage({
@@ -17,12 +34,9 @@ const storage = multer.diskStorage({
         cb(null, file.fieldname);
     }
 });
+
 let upload = multer({ storage});
-
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
+ 
 app.get("/" , function(req, res){
     res.render("home");
 });
@@ -37,17 +51,45 @@ app.get("/" , function(req, res){
 
 app.post('/', upload.single('csv-file'), (req, res, next) => {
     const file = req.file
+    console.log(file);
     if (!file) {
       const error = new Error('Please upload a file')
       error.httpStatusCode = 400
       return next(error)
     }
-    res.send("Your files has been saved"); 
+
+    csv().fromFile(req.file.path)
+    .then(function (jsonObj) {
+        console.log(jsonObj);
+
+
+        Bookycontact.insertMany(jsonObj, function(err, docs){
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect('/');
+            }
+        })
+    });
+
+    
+
+    // TODO: validate file and return error if any
+    // TODO: for each line in file, add to db.
+
+    // res.send("Your files has been saved"); 
 })
 
-//${__dirname}/public/${req.file.filename}
 
+// TODO: connect to db. If error, abort
+// const dbURI = "mongodb://localhost:27017/contacts"
+// mongoose.connect(dbURI).then(() => [
+    
+// ]).catch(err => {
+//     console.error('Failed to connect to mongodb. Aborting', err)
+// })
 
+// start listening
 app.listen(3000, function(){
     console.log("Server started on port 3000");
-});
+})
