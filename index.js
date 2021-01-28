c27bbec8ea40762787865725397465c9252a06ac
frankie-njie <njie_frankie@yahoo.com>
2021-01-28 18:29:13 +0100

    some changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const csv = require('csvtojson');
 // const path = require('path');
 
 const BookycontactModel = require("./models/BookyContact");
-const {json} = require('body-parser');
+//const {json} = require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -46,38 +46,93 @@ app.post('/', upload.single('csv-file'), (req, res, next) => {
 
     csv().fromFile(req.file.path)
     .then(function (jsonObj) {
-        console.log("jsonObj: ", jsonObj);
-    
+        // console.log("jsonObj: ", jsonObj);
 
+    // TODO: Required fields
+    const reqFields = ['email', 'first name', 'last name', 'phone'];
+    const CONTACT_NAMES = ['first name','last name'];
+    const validateContactFields = arr => {
+        arr = arr.map(ele => ele.toLowerCase());
+        let status = true;
+
+        for (let index = 0; index < reqFields.length; index++) {
+            if(!arr.includes(reqFields[index])) {
+                status = false;
+                break;
+            }
+        }
+
+        return status;
+    }
 
     // TODO: for each line in file, add to db
         if (jsonObj && jsonObj.length > 0){
-            jsonObj.forEach((contact, index) => {
-                // validation for email address 
-                if(contact["Email Address"] || contact.email === ""){
-                    json.slice(index);
-                    console.log("This element does not have and email address", contact);
+            jsonObj.forEach(contact => {
+                
+                if(validateContactFields(Object.keys(contact)) && contact.email ){
+                    const newContact = {}
+
+                    Object.keys(contact).map(field => {
+
+                        const name = field.toLowerCase();
+
+                        
+                        if(CONTACT_NAMES.includes(name) ){
+                            newContact[name[0] + "Name" ] = contact[field]
+                        }
+                        else newContact[name] = contact[field]
+                    })
+
+                    console.log(newContact);
                 }
 
-                console.log("> Insert : ", contact["Email Address"] || contact.email);
+                else console.log(contact)
 
-                const book = {
-                    "fName" : contact["First Name"] || contact["first Name"],
-                    "lName" : contact["Last Name"] || contact["last Name"],
-                    "email" : contact["Email Address"] || contact.email,
-                    "phoneNum" : contact["Phone Number"] || contact.phone ,
-                    "Sex": "" ||  contact.Sex
-                }
-                console.log(book);
-
-                const booky = new BookycontactModel(book);
+                
 
 
-                booky.save((err) => {
-                    if (err) throw err;
-                    else console.log("> Saved !");
-                });
-            });
+
+            })
+            // jsonObj.forEach((contact, index) => {
+
+            //     const contactsKeys = Object.keys(contact);
+            //     // validate
+            //     contactsKeys.map(con => {
+            //         if(reqFields.includes(con.toLowerCase()))
+            //             console.log()
+
+            //         // else console.error(contact)
+                    
+            //     })
+
+            //     // // validation for email address 
+            //     // if(contact["Email Address"] || contact.email === ""){
+            //     //     contact.slice(index);
+            //     //     console.log("This element does not have and email address, email address is required", contact);
+            //     // }else if (!(contact["First Name"] || contact["first Name"])){
+            //     //     console.log(contact);
+                    
+            //     // }
+
+            //     // console.log("> Insert : ", contact["Email Address"] || contact.email);
+
+            //     // const book = {
+            //     //     "fName" : contact["First Name"] || contact["first Name"],
+            //     //     "lName" : contact["Last Name"] || contact["last Name"],
+            //     //     "email" : contact["Email Address"] || contact.email,
+            //     //     "phoneNum" : contact["Phone Number"] || contact.phone ,
+            //     //     "Sex": "" ||  contact.Sex
+            //     // }
+            //     // console.log(book);
+
+            //     // const booky = new BookycontactModel(book);
+
+
+            //     // booky.save((err) => {
+            //     //     if (err) throw err;
+            //     //     else console.log("> Saved !");
+            //     // });
+            // });
         }else{
             console.log("[x] Empty csv file !")
         }
