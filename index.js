e4037873859257099c9bb1136692cc439a05837e
frankie-njie <njie_frankie@yahoo.com>
2021-01-28 11:51:22 +0100

    added validation for email

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const ejs = require('ejs');
 const multer = require('multer');
 const csv = require('csvtojson');
-const path = require('path');
+// const path = require('path');
 
 const BookycontactModel = require("./models/BookyContact");
+const {json} = require('body-parser');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -31,13 +32,6 @@ app.get("/" , function(req, res){
     res.render("home");
 });
 
-// app.post("/", function(req, res){
-//      console.log(upload);
-//      res.render("file")
-//     //  res.render("file",function(req, res){
-//     //     res.send("Your files has been saved");
-//     //  })
-// })
 
 app.post('/', upload.single('csv-file'), (req, res, next) => {
     const file = req.file
@@ -53,20 +47,31 @@ app.post('/', upload.single('csv-file'), (req, res, next) => {
     csv().fromFile(req.file.path)
     .then(function (jsonObj) {
         console.log("jsonObj: ", jsonObj);
+    
 
+
+    // TODO: for each line in file, add to db
         if (jsonObj && jsonObj.length > 0){
             jsonObj.forEach((contact, index) => {
-                console.log("> Insert : ", contact["Email Address"]);
-
-                const book = {
-                    "fName" : contact["First Name"],
-                    "lName" : contact["Last Name"],
-                    "email" : contact["Email Address"],
-                    "phoneNum" : contact["Phone Number"],
-                    "Sex": ""
+                // validation for email address 
+                if(contact["Email Address"] || contact.email === ""){
+                    json.slice(index);
+                    console.log("This element does not have and email address", contact);
                 }
 
+                console.log("> Insert : ", contact["Email Address"] || contact.email);
+
+                const book = {
+                    "fName" : contact["First Name"] || contact["first Name"],
+                    "lName" : contact["Last Name"] || contact["last Name"],
+                    "email" : contact["Email Address"] || contact.email,
+                    "phoneNum" : contact["Phone Number"] || contact.phone ,
+                    "Sex": "" ||  contact.Sex
+                }
+                console.log(book);
+
                 const booky = new BookycontactModel(book);
+
 
                 booky.save((err) => {
                     if (err) throw err;
@@ -78,7 +83,6 @@ app.post('/', upload.single('csv-file'), (req, res, next) => {
         }
     });
 
-    // TODO: for each line in file, add to db
     res.send("Your files has been saved");
 })
 
